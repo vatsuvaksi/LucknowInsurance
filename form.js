7e1f6aa0ab796b5b120eb621e13dc225a39caf9c
vatsuvaksi <62776100+vatsuvaksi@users.noreply.github.com>
2020-10-25 23:02:27 +0530

    Final push for the client

---
+++ form.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+if (window.confirm('Kindly fill this form so that we can assist you better! or click on cancel if you have already submitted the details')) 
+{
+window.location.href='https://docs.google.com/forms/d/e/1FAIpQLSejOx65nmL8t59_buovLd57tidZuRnKg7ZvlxMH2WZZmjfjng/viewform?usp=sf_link';
+};
+});
